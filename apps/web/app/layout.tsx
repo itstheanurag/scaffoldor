7de00da56b777a456b40585983b0a6c5a99f8c14
Gaurav Kumar <gauravanurag36@gmail.com>
2026-02-10 10:27:28 +0530

    add fevicon and metadata required for the app

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -16,6 +16,36 @@ export const metadata: Metadata = {
   title: "Scaffoldor - Download templates from GitHub with ease",
   description:
     "The easiest way to scaffold your projects using templates from GitHub. Supports multiple frameworks and package managers.",
+  alternates: {
+    canonical: "https://scaffoldor.vercel.app",
+  },
+  openGraph: {
+    title: "Scaffoldor - Download templates from GitHub with ease",
+    description:
+      "Scaffold your projects using templates from GitHub with ease.",
+    url: "https://scaffoldor.vercel.app",
+    siteName: "Scaffoldor",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "/scaffoldor.png",
+        width: 1200,
+        height: 630,
+        alt: "Scaffoldor - Projects scaffolding made easy",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Scaffoldor - Download templates from GitHub with ease",
+    description:
+      "Scaffold your projects using templates from GitHub with ease.",
+    images: ["/scaffoldor.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
